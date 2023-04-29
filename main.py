a08b5edc62b77aa8c29f082d5f58872a47fb0efa
pkncoder <128310642+pkncoder@users.noreply.github.com>
2023-04-29 18:36:05 +0000

    Getting ready to finnish Letter to Letter Grade Average.

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
-    usrch = input('Would you like to use Letter to GPA(LetToGPA), or Number to Letter(NumToLet)?')
+usrch = input('Would you like to use Letter to GPA(LetToGPA), or Number to Letter grade average(NumToLet)?')
 
-    if usrch == 'LetToGPA':
-        fin = 0
-        i = 0
-        cont = True
-        num = 0
+if usrch == 'LetToGPA':
+    fin = 0
+    i = 0
+    cont = True
+    num = 0
 
-        grades = {
-            'A':4,
-            'B':3,
-            'C':2,
-            'D':1,
-            'F':0,
-        }
+    gradesGPA = {
+        'A':4,
+        'B':3,
+        'C':2,
+        'D':1,
+        'F':0,
+    }
+    gradesLetNum = {
+        100 <= 90: 'A',
+        89 <= 80: 'B',
+        79 <= 70: 'C',
+        69 <= 60: 'D',
+        59 <= 0: 'F',
+    }
 
-        print("Hello, welcome to the gpa, or grade-point-average calculator. This calculator takes letter grades and turns them into a gpa. If you don't know what a gpa means or converts to it is simple. 4 to 3.1 is an A, 3 to 2.1 is a B, 2 to 1.1 is a D, and a 1 to 0 is an F.")
+    print("Hello, welcome to the gpa, or grade-point-average calculator. This calculator takes letter grades and turns them into a gpa. If you don't know what a gpa means or converts to it is simple. 4 to 3.1 is an A, 3 to 2.1 is a B, 2 to 1.1 is a D, and a 1 to 0 is an F.")
 
-        while cont != False:
-            grd = input('Grade: ')
-        
-            if grd == 'done':
-                cont = False
-            elif grd == 'A' or 'B' or 'C' or 'D' or 'F':
-                num += grades[grd]
-                i += 1
-                num += fin
-            else:
-                print("Incorrect input, try again.")
+    while cont != False:
+        grd = input('Grade: ')
+    
+        if grd == 'done':
+            cont = False
+        elif grd == 'A' or 'B' or 'C' or 'D' or 'F':
+            num += gradesGPA[grd]
+            i += 1
+            num += fin
+        else:
+            print("Incorrect input, try again.")
 
-        gpa = num / i
+    gpa = num / i
 
-        print(f'{gpa:.2f}')
-    elif usrch == 'NumToLet':
-        print('')
+    print(f'{gpa:.2f}')
+elif usrch == 'NumToLet':
+    print('Hello, welcome to a letter(s) to letter grade average calculator. Type "done" when done.')
+    while cont != False:
+        grd = int(input("Please input your grade (int only): "))
+        if grd == 
