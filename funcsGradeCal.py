f637c6ac7b38bd8a219b74b27fb4f4551c699312
pkncoder <alex.h.preston@gmail.com>
2023-04-30 19:17:56 -0400

    Hello welcome to the grade calculator's functions!

--- funcsGradeCal.py
+++ funcsGradeCal.py
@@ -1,13 +1,19 @@
 def NumToGPAFunc(grd):
+
     if grd >= 90:
         return 'A'
+    
     elif grd >= 80:
         return 'B'
+    
     elif grd >= 70:
         return 'C'
+    
     elif grd >= 60:
         return 'D'
+    
     elif grd >= 0:
         return 'F'
+    
     else:
         return 'done'
