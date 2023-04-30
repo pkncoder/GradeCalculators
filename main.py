770759ce7322525db98387ad374ffcb291952f5b
pkncoder <128310642+pkncoder@users.noreply.github.com>
2023-04-30 20:31:21 +0000

    It is working now. The current vertion is now working with a letter to GPA, and a Percent to GPA. Cheers.

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from funcsGradeCal import NumToGPAFunc
-
 usrch = input('Would you like to use Letter to GPA(LetToGPA), or Number to GPA (NumToGPA)?\n')
-
 gradesGPA = {
         'A':4,
         'B':3,
@@ -10,18 +8,14 @@ gradesGPA = {
         'F':0,
         'done': 'done'
     }
-
 if usrch == 'LetToGPA':
     fin = 0
     i = 0
     cont = True
     num = 0
-
     print("Hello, welcome to the gpa, or grade-point-average calculator. This calculator takes letter grades and turns them into a gpa. If you don't know what a gpa means or converts to it is simple. 4 to 3.1 is an A, 3 to 2.1 is a B, 2 to 1.1 is a D, and a 1 to 0 is an F.")
-
     while cont != False:
         grd = input('Letter grade: ')
-
         if grd == 'done':
             cont = False
         elif grd == 'A' or 'B' or 'C' or 'D' or 'F':
@@ -30,35 +24,23 @@ if usrch == 'LetToGPA':
             num += fin
         else:
             print("Incorrect input, try again.")
-
     gpa = num / i
-
     print(f'{gpa:.2f}')
 elif usrch == 'NumToGPA':
     cont = True
     i = 0
-
+    NUM = 0
     print('Hello, welcome to a percent to GPA calculator. Type a negitive number when done.')
-
-    while cont != False:
+    while cont == True:
         grd = int(input("Please input your grade (int only): "))
-        num = 0
-        i = 0
-        fin = 0
-
         if grd <= 100:
-            i += 1
-
-            num1 = NumToGPAFunc(grd)
-            print(num1)
-
-            print(num1)
-            if num1 == 'done':
+            NUM1 = NumToGPAFunc(grd)
+            if NUM1 == 'done':
                 cont = False
             else:
-                num += gradesGPA[num1]
-                num += fin
+                i += 1
+                NUM += gradesGPA[NUM1]
         else:
             print('Incorrect Input')
-    gpa = num / i
-    print(gpa)
+    gpa = NUM / i
+    print(f'{gpa:.2f}')
